69762487030b56ae8f65ce1ee4270fa777fec1e7
Billie Devolder <devolder.billie96@gmail.com>
2021-12-01 18:16:11 +0100

    Add shorter solution for day01

--- day01/solution.kts
+++ day01/solution.kts
@@ -8,15 +8,14 @@ val lines = File("input.txt")
 
 fun part1(lines: List<Int>): Int {
     return lines
-        .zipWithNext()
-        .filter { it.second > it.first }
-        .count()
+        .windowed(2)
+        .count { it.first() < it.last() }
 }
 
 fun part2(lines: List<Int>): Int {
-    return (0 until lines.size - 3)
-        .filter { i -> lines[i] < lines[i + 3] }
-        .count()
+    return lines
+        .windowed(4)
+        .count { it.first() < it.last() }
 }
 
 println("Part 1: ${part1(lines)}")
